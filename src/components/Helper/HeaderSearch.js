f3e09e08fe606c99cd27487c245f39aab2196dac
razaZainikLab <rakibraza.zainiklab@gmail.com>
2022-04-12 17:06:41 +0600

    search done

--- src/components/Helper/HeaderSearch.js
+++ src/components/Helper/HeaderSearch.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
-
-import { useCategoryContext } from '../../context/CategoryContext';
-import search from '../../assets/images/search.png';
 import axios from 'axios';
+
+import search from '../../assets/images/search.png';
 import { useAuthContext } from '../../context/AuthContext';
 
 const HeaderSearch = () => {
   const { API } = useAuthContext()
   const { t } = useTranslation()
   const navigate = useNavigate()
-  const { scripts, getAllScripts } = useCategoryContext()
   const [filterdata, setFilterdata] = useState([]);
   const [wordInter, setWordInter] = useState('');
 
   const handleFilter = async (e) => {
     const searchWord = e.target.value
     setWordInter(searchWord)
-    // const res = await axios.get(`${API}/compare/${searchWord}`)
-    const newFilter = scripts.filter((item) => item.title.toLowerCase().includes(searchWord.toLowerCase()))
     if (searchWord) {
-      setFilterdata(newFilter)
+      const res = await axios.get(`${API}/search/scripts/${searchWord}?limit=5`)
+      setFilterdata(res.data.data.data)
+      console.log(e.target.value)
     } else {
+      console.log('empty')
       setFilterdata([])
     }
   }
@@ -31,20 +30,16 @@ const HeaderSearch = () => {
     setFilterdata([])
     setWordInter("")
   }
-  useEffect(() => {
-    getAllScripts()
-  }, [])
 
   return <div className="header-search position-relative">
     <div className="serch-box position-relative">
       <input onChange={handleFilter} value={wordInter} type="text" placeholder={t("search_scrippts")} autoComplete="off" />
       <button>
         {wordInter ? <FaTimes onClick={clearInput} /> : <img src={search} alt="search" />}
-        {/* <img src={search} alt="search" /> */}
       </button>
     </div>
     {filterdata.length !== 0 && <div className="data-result">
-      {scripts && filterdata.slice(0, 15).map(({ title, id, slug }) => (
+      {filterdata.map(({ title, id, slug }) => (
         <div key={id} onClick={() => {
           setFilterdata([])
           setWordInter("")
